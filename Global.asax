e86f5c25cbab692b4775800fd16f7a611fa69d44
Leber <sl854216@ohio.edu>
2021-03-03 11:02:47 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Centric_Project.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Centric_Project.Startup))]
+namespace Centric_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
