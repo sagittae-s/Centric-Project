e66a3c1b60fe6fbcea26863ff9fc394fcf75320a
Eileen Guzik <eg909116@ohio.edu>
2021-03-04 12:23:47 -0500

    Created header names for each view, displayed the full name rather than first and last, and added some bootstrap formatting.

--- Models/userData.cs
+++ Models/userData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,14 @@ namespace Centric_Project.Models
     public class userData
     {
         public Guid ID { get; set; }
+
+        [Display(Name ="First Name")]
         public string firstName { get; set; }
+
+        [Display(Name ="Last Name")]
         public string lastName { get; set; }
+
+        [Display(Name ="Full Name")]
         public string fullName 
         { 
             get
@@ -17,8 +24,14 @@ namespace Centric_Project.Models
                 return lastName + ", " + firstName;
             }
                 }
+
+        [Display(Name ="Business Unit")]
         public string businessUnit { get; set; }
+
+        [Display(Name ="Hire Date")]
         public DateTime hireDate { get; set; }
+
+        [Display(Name ="Title")]
         public string title { get; set; }
     }
 }
