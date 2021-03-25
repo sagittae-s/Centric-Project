a31afcca09440910c77e6783ec2946e4defde3b3
Kendall Black <kb305616@ohio.edu>
2021-03-25 19:38:36 -0400

    Created model for recognition, haven't migrated anything yet.

---
+++ Models/Recognition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Centric_Project.Models
+{
+    public class Recognition
+    {
+        public int recognitionID { get; set; }
+        public Guid ID { get; set; }
+        [Display(Name = "Core Value")]
+        public string coreValues { get; set; }
+        [Display(Name = "Reason")]
+        public string reason { get; set; }
+        public ICollection<userData> userDatas { get; set; }
+    }
+}
